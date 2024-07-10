ceec1dcc0706da73b3cc55974bb5a00555dda63f
rohajo <rhjonsson@posteo.de>
2024-07-10 17:52:27 +0200

    fixed run_test_job.sh

--- run_test_job.sh
+++ run_test_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate cfs_v2024_env
-python /src/run.py --f=2 --n=1 --m=16 --seed=4
+python ./src/run.py --f=2 --n=1 --m=16 --seed=4

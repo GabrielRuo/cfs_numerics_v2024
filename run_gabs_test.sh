2a84baef41360606905292e8eb609e45f1e99828
rohajo <74736624+rohajo@users.noreply.github.com>
2024-07-10 20:02:27 +0200

    Update run_gabs_test.sh: debug try

--- run_gabs_test.sh
+++ run_gabs_test.sh
@@ -27,4 +27,7 @@ fi
 script_dir="$(dirname "$0")"
 
 # Launching jobs for all experiments
-python "${script_dir}/src/cluster_submit.py" --experiment_name=n1_testing --config=gabs_test --n_runs 1 --runpath="${script_dir}/src/run.py"
+# python "${script_dir}/src/cluster_submit.py" --experiment_name=n1_testing --config=gabs_test --n_runs 1 --runpath="${script_dir}/src/run.py"
+python "${script_dir}/src/cluster_submit.py" --experiment_name=n1_testing --config=gabs_test --n_runs 1 --runpath=/home/x_rojon/cfs_numerics_v2024/src/run.py
+
+

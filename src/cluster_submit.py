30fdbdab4dc86ff415f2172a95e2989a806cd83c
rohajo <74736624+rohajo@users.noreply.github.com>
2024-07-11 13:11:30 +0200

    Update cluster_submit.py: added lines for module load and conda env to run_job.cmd

--- src/cluster_submit.py
+++ src/cluster_submit.py
@@ -133,6 +133,27 @@ def submit_all_jobs(args: Sequence[Dict[Text, Any]], config) -> None:
     lines.append(f"#SBATCH -o {logs_dir}_%a/slurm.out")
     lines.append(f"#SBATCH -e {logs_dir}_%a/slurm.err")
 
+    # Module purge and load (could be specific to Tetralith)
+    module purge
+    module load Anaconda/2023.09-0-hpc1
+
+    ## Activate Conda environment (assuming conda is properly initialized, and environment cfs_v2024_env created from setup_env.sh)
+    # ChatGPT said: It's good practice to include error handling if conda.sh cannot be found
+    if [ -f "$(conda info --base)/etc/profile.d/conda.sh" ]; then
+      source $(conda info --base)/etc/profile.d/conda.sh
+    else
+      echo "Error: conda.sh not found. Make sure Conda is properly installed and initialized."
+      exit 1
+    fi
+    # Activate the specific Conda environment
+    conda activate cfs_v2024_env
+
+    # Check if activation was successful
+    if [ $? -ne 0 ]; then
+      echo "Error: Failed to activate Conda environment 'cfs_v2024_env'."
+      exit 1
+    fi
+
     # Queue job
     lines.append("")
     runcmd = executable
